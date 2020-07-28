11a70fd9b2354e88e4fa16757a9fcff61c7e65ee
vurumutu <wojciech.zgliniecki@gmail.com>
2020-07-28 22:01:36 +0200

    Dark/Light Mode fixed.

--- js/modes.js
+++ js/modes.js
@@ -1,3 +1,4 @@
+/*
 const btn = document.querySelector(".btn-mode-toggle");
 const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
 
@@ -22,4 +23,22 @@ btn.addEventListener("click", function () {
   }
   localStorage.setItem("theme", theme);
 });
+*/
+const toggleColorMode = e =>{
+    if (e.currentTarget.classList.contains("light--hidden")){
+      document.documentElement.setAttribute("theme", "light-theme");
 
+      localStorage.setItem("theme", "light-theme");
+      return;
+    }
+
+    document.documentElement.setAttribute("theme", "dark-theme");
+
+    localStorage.setItem("theme", "dark-theme");
+};
+
+const toggleColorButtons = document.querySelectorAll(".btn-mode-toggle")
+
+toggleColorButtons.forEach(btn =>{
+  btn.addEventListener("click", toggleColorMode);
+});
